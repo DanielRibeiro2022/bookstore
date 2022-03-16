b61276e2c43a7681baa824d2d5aa47d463d553a7
DanielRibeiro2022 <danri1971@gmail.com>
2022-03-16 17:27:20 -0300

    tratamento de error personalizado

--- src/main/java/br/daniel/bookstore/service/CategoriaService.java
+++ src/main/java/br/daniel/bookstore/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import br.daniel.bookstore.domain.Categoria;
 import br.daniel.bookstore.repositories.CategoriaRepository;
+import br.daniel.bookstore.service.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,7 @@ public class CategoriaService {
 	
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " +Categoria.class.getName()));
 		
 	}
 }
