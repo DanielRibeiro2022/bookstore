486a4cd721fcf4fc90c15fc849278c71e9592aac
DanielRibeiro2022 <danri1971@gmail.com>
2022-03-17 16:26:46 -0300

    FindAll para categoria

--- src/main/java/br/daniel/bookstore/resources/CategoriaResource.java
+++ src/main/java/br/daniel/bookstore/resources/CategoriaResource.java
@@ -1,5 +1,8 @@
 package br.daniel.bookstore.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,20 +11,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.daniel.bookstore.domain.Categoria;
+import br.daniel.bookstore.dtos.CategoriaDTO;
 import br.daniel.bookstore.service.CategoriaService;
 
 @RestController
-@RequestMapping(value= "/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
+
 	@Autowired
 	private CategoriaService service;
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Categoria> findById(@PathVariable Integer id){
+	public ResponseEntity<Categoria> findById(@PathVariable Integer id) {
 		Categoria obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
-	
+
+	@GetMapping
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
 }
--- src/main/java/br/daniel/bookstore/service/CategoriaService.java
+++ src/main/java/br/daniel/bookstore/service/CategoriaService.java
@@ -1,5 +1,6 @@
 package br.daniel.bookstore.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +18,12 @@ public class CategoriaService {
 	
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " +Categoria.class.getName()));
-		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " +Categoria.class.getName()));	
 	}
+	
+	public List<Categoria> findAll(){
+		return repository.findAll();
+	}
+	
+	
 }
